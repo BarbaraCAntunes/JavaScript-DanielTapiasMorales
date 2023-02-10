bd0d21241dc09b0344ef1624a8c1998f3bfc45e8
Barbara <barbaracantunes@gmail.com>
2023-02-10 14:28:30 -0300

    Exercicio (des)habilita propriedades com JS

---
+++ DOM_edit_textfield_original/js/newsletter.js
@@ -0,0 +1,8 @@
+const txtEmail = document.getElementById('txtEmail');
+function editarEmail() {
+  txtEmail.disabled = false;
+  txtEmail.focus();
+}
+function desabilitaEmail() {
+  txtEmail.disabled = true;
+}
